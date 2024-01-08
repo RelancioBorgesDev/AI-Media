30ded5137b7ab5d5a03609a367d26cd5a32661b6
RelancioBorgesDev <relancio.dev@gmail.com>
2024-01-08 12:29:42 -0300

    Estrutura inicial com o shadcn/ui

---
+++ src/components/logo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <span className="bg-zinc-950 p-2 rounded-full text-2xl font-bold text-white">
+      AI Media
+    </span>
+  );
+}
